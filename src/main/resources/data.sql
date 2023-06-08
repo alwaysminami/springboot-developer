195888597473d3102095ca0e5fa94f7fb9b9f681
alwaysminami <kimyesung2000@gmail.com>
2023-06-08 09:41:22 +0900

    6.5.1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content) VALUES ('제목 1', '제목 1')
+INSERT INTO article (title, content) VALUES ('제목 2', '제목 2')
+INSERT INTO article (title, content) VALUES ('제목 3', '제목 3')
